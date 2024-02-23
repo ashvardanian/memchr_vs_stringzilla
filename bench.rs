419864cbb2eedbb56ee8976f4a6cd9c948b2d081
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2024-02-23 21:57:27 +0000

    Fix: Aggregate benchmarks for dif. tokens

--- bench.rs
+++ bench.rs
@@ -1,15 +1,22 @@
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use std::env;
 use std::fs;
-use std::str;
 
+use memchr::memmem;
+use stringzilla::StringZilla;
+
+fn configure_bench() -> Criterion {
+    Criterion::default()
+        .sample_size(1000) // Test this many needles.
+        .warm_up_time(std::time::Duration::from_secs(10)) // Let the CPU frequencies settle.
+        .measurement_time(std::time::Duration::from_secs(120)) //
+}
 
 fn benchmarks(c: &mut Criterion) {
-    use stringzilla::StringZilla;
-
-    // Get the document path from the first command line argument.
-    let document_path = env::var("DOCUMENT_PATH").expect("DOCUMENT_PATH environment variable not set");
-    let document_content = fs::read_to_string(document_path).expect("Could not read file");
+    // Get the document path from the environment variable.
+    let document_path =
+        env::var("DOCUMENT_PATH").expect("DOCUMENT_PATH environment variable not set");
+    let document_content = fs::read_to_string(&document_path).expect("Could not read file");
 
     // Tokenize the document content by white space.
     let tokens: Vec<&str> = document_content.split_whitespace().collect();
@@ -19,31 +26,38 @@ fn benchmarks(c: &mut Criterion) {
 
     let file = document_content.as_bytes();
 
-    let mut group = c.benchmark_group("TokenSearch");
-    for (i, token) in tokens.iter().enumerate() {
-        let token_bytes = token.as_bytes();
+    // Benchmark for StringZilla
+    let mut token_index = 0u64; // Token index outside of the iter closure
+    c.bench_function("stringzilla", |b| {
+        b.iter(|| {
+            let token = tokens[token_index];
+            let token_bytes = token.as_bytes();
+            let mut pos = 0u64;
+            while let Some(found) = (&file[pos..]).sz_find(token_bytes) {
+                pos += found + token_bytes.len(); // Move past the found token for the next search.
+            }
+            token_index = (token_index + 1) % tokens.len(); // Move to the next token, wrap around if necessary
+        })
+    });
 
-        group.bench_function(BenchmarkId::new("stringzilla", i), |b| {
-            b.iter(|| {
-                let mut pos = 0;
-                while let Some(found) = (&file[pos..]).sz_find(token_bytes) {
-                    pos += found + token_bytes.len(); // Move past the found token for the next search.
-                }
-            })
-        });
-
-        group.bench_function(BenchmarkId::new("memchr", i), |b| {
-            b.iter(|| {
-                let mut pos = 0;
-                while let Some(found) = memchr::memmem::find(&file[pos..], token_bytes) {
-                    pos += found + token_bytes.len(); // Move past the found token for the next search.
-                }
-            })
-        });
-    }
-
-    group.finish();
+    // Benchmark for memchr
+    let mut token_index = 0u64; // Reset token index for the next benchmark
+    c.bench_function("memchr", |b| {
+        b.iter(|| {
+            let token = tokens[token_index];
+            let token_bytes = token.as_bytes();
+            let mut pos = 0u64;
+            while let Some(found) = memmem::find(&file[pos..], token_bytes) {
+                pos += found + token_bytes.len(); // Move past the found token for the next search.
+            }
+            token_index = (token_index + 1) % tokens.len(); // Move to the next token, wrap around if necessary
+        })
+    });
 }
 
-criterion_group!(sz_bench, benchmarks);
+criterion_group! {
+    name = sz_bench;
+    config = configure_bench();
+    targets = benchmarks
+}
 criterion_main!(sz_bench);
